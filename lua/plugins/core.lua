b3c0663fa29338f68f8e57dbc589e76d205b0ef6
Tommy Crews <tommyc@phase2online.com>
2025-01-09 13:52:33 -0600

    Update config.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,14 @@
 return {
   {
+    "https://codeberg.org/esensar/nvim-dev-container",
+    config = function()
+      require("devcontainer").setup({})
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-    }
-  }
+    },
+  },
 }
